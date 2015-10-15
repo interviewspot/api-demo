16567c28af7a64f00eaccce3ac0418b933f8da0d
Lê Thanh Bình <binh@sunrise.vn>
2015-10-16 01:20:51 +0700

    new testing

--- api/core/user/user.js
+++ api/core/user/user.js
@@ -13,11 +13,46 @@ QUnit.test("hello test", function (assert) {
         'x-username': Setting.username,
         'x-password': Setting.password
     });
-    a.navigate(['organisations','first']);
+    //a.navigate(['organisations']);
+    //a.get(function (response) {
+    //    var obj = response.getValue();
+    //    console.log('first');
+    //    console.log(obj);
+    //    console.log(response.isOk());
 
-    a.get(function (response) {
-        console.log(response);
-        console.log(response.isOk());
-    });
+        //
+        //a.navigate([{
+        //    'id': 2
+        //},'self','medium.logo.update']);
+        //
+        //a.get(function (response1) {
+        //    var obj = response1.getValue();
+        //    console.log('hello');
+        //    console.log(obj);
+        //});
+    //
+    //});
+
+    var data = {
+        "adminUser": "", //id of user
+        "parent": "", //id of parent
+        "location": "", //id of location
+        "logo": "", //id of logo
+        "name": "1",
+        "code": "12345678",
+        "regNo": "1",
+        "head_office_no": "1",
+        "officeAddress": "1",
+        "billingAddress": "1",
+        "reservationEmail": "1",
+        "userContactNo": "1",
+        "client_since": "2015-01-01 10:10:10",
+        "officeHours": "1",
+        "redemptionPassword": "1",
+        "aboutCompany": "1"
+    };
+    a.navigate(['organisations.post']);
+    a.post(function(res){console.log(res);},{organisation: data});
+
 
 });
